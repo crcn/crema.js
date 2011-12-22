9df63c96c48afd937f39a7f4e6429706f2ab2f47
Craig Condon <craig@spiceapps.com>
2011-12-22 15:44:22 -0600

    fixed issue with midddleware

--- examples/index.js
+++ examples/index.js
@@ -3,7 +3,8 @@ var crema = require('../lib');
 
 var route = 'request -method=POST OR -method=GET (hello world) OR (hello earth) -> google';
 
-// route = 'request -method=GET  authorize -> login';
+
+route = 'request -method=GET  google/goggle/giggle OR test -> test/authorize -> login';
 
 
 console.log(JSON.stringify(crema(route), null, 2));
